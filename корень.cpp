fc68a45118de64b277090d84d4cfa3de0c2b1d46
waweka <xasam55@mail.ru>
2017-04-07 22:20:01 +0300

    Add files via upload

---
+++ корень.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "number = ";
+	cin >> a;
+	
+	if((sqrt(a))>= 0 )
+	{
+		
+		b = (sqrt(a) );
+		cout << " number is = " << b << endl;
+	}
+	else
+	{
+		cout << " Non" << endl;
+	}
+	return 0;
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
